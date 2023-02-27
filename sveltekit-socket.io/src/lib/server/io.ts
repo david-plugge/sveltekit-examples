09325bf2d2ed9f6c94d4048f11bfc41be489741e
David Plugge <git@davidplugge.de>
2023-02-27 11:44:50 +0100

    create chat example

--- sveltekit-socket.io/src/lib/server/io.ts
+++ sveltekit-socket.io/src/lib/server/io.ts
@@ -5,12 +5,30 @@ export const io = new Server<ClientToServerEvents, ServerToClientEvents, ServerT
 io.on('connection', (socket) => {
 	console.log('connection', socket.id);
 
-	socket.emit('test');
+	socket.on('post:create', (text, callback) => {
+		// simulate error
+		if (Math.random() > 0.8) {
+			return callback({
+				success: false
+			});
+		}
+
+		socket.broadcast.emit('post:create', text);
+		callback({
+			success: true,
+			text
+		});
+	});
 });
 
 export interface ServerToClientEvents {
-	test(): void;
+	'post:create'(text: string): void;
 }
-export interface ClientToServerEvents {}
+export interface ClientToServerEvents {
+	'post:create'(
+		text: string,
+		callback: (result: { success: false } | { success: true; text: string }) => void
+	): void;
+}
 export interface ServerToServerEvents {}
 export interface SocketData {}
--- sveltekit-socket.io/src/routes/+page.svelte
+++ sveltekit-socket.io/src/routes/+page.svelte
@@ -1,2 +1,85 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { socket, wsOn } from '$lib/socket';
+	import { tick } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	let newPost = '';
+	let sending = false;
+	let error: string | null = null;
+	let inputElement: HTMLInputElement;
+
+	const maxRenderedPosts = 100;
+	const posts = writable<string[]>([]);
+
+	function addPost(text: string) {
+		posts.update(($posts) => {
+			$posts.unshift(text);
+			if ($posts.length > maxRenderedPosts) {
+				$posts.splice(maxRenderedPosts, Infinity);
+			}
+			return $posts;
+		});
+	}
+
+	wsOn('post:create', addPost);
+
+	function handleSend() {
+		if (!newPost) return;
+		sending = true;
+		error = null;
+		socket.timeout(1000).emit('post:create', newPost, (err, result) => {
+			sending = false;
+			if (err) {
+				error = err.message;
+			} else if (!result.success) {
+				error = 'Could not send message';
+			} else {
+				addPost(result.text);
+			}
+			tick().then(() => inputElement.focus());
+		});
+		newPost = '';
+	}
+	function handleKeydown(e: KeyboardEvent) {
+		if (e.key === 'Enter') {
+			handleSend();
+		}
+	}
+</script>
+
+<input
+	bind:this={inputElement}
+	disabled={sending}
+	type="text"
+	bind:value={newPost}
+	on:keydown={handleKeydown}
+/>
+<button disabled={sending} on:click={handleSend}>Send</button>
+
+{#if error}
+	<pre>{JSON.stringify(error, null, 2)}</pre>
+{/if}
+
+<div>
+	{#each $posts as post}
+		<p>{post}</p>
+	{/each}
+</div>
+
+<style>
+	div {
+		border: 1px solid #eee;
+		padding: 0.5em;
+		margin: 0.5em 0;
+		display: flex;
+		flex-direction: column;
+		gap: 0.25em;
+		max-height: 800px;
+		overflow: auto;
+	}
+	p {
+		padding: 0.5em;
+		margin: 0;
+		background-color: #eee;
+	}
+</style>
